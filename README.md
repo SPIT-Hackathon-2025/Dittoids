6edd6d7c2a3b0e77da2a6b96f61fbe3352307616
Dimsas04 <siddheshshrawne10@gmail.com>
2025-02-08 14:39:18 +0530

    added assignment functionality

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Siddhesh:
 1) Ticket Creation with Email (Easy)
 2) Ticket Resolution (Easy)
 3) Calenders for every employee (Maybe Tricky)
-4) Chatbot:
-    a) Create meeting time recommendations based on free time
-    b) 
+4) Agents:
+    i) Pavlov (Task Management):
+        a) Create Zendesk Tickets
+        b) Track pending tickets and their status
+        c) Generate a list of tasks completed today
+    ii) Andrei (Executive Assistant):
+    iii) Nikolai (Calendar Agent):
--- venv/zendeskUtils.py
+++ venv/zendeskUtils.py
@@ -2,6 +2,7 @@ import requests
 import json
 from dotenv import load_dotenv
 import os
+from pymongo import *
 
 load_dotenv()
 
@@ -10,26 +11,52 @@ DOMAIN = os.getenv("DOMAIN")
 EMAIL = os.getenv("EMAIL")
 
 
+client = MongoClient(os.getenv("MONGO_CONN_STR"))
+db = client['Dittoids']  # Create or access the database
+
 
 
 def create_zendesk_ticket(
-    subject, description, requester_name, requester_email, assigneeID
+    subject, description, requester_name, requester_email, assigneeEmail
 ):
-
-    url = f"https://{DOMAIN}.zendesk.com/api/v2/requests.json"
+    assigneeID=get_agent_id_by_email(assigneeEmail)
+    url = f"https://{DOMAIN}.zendesk.com/api/v2/tickets.json"
     headers = {"Content-Type": "application/json"}
-    data = {
-        "request": {
+    # data = {
+    #     "request": {
+    #         "subject": subject,
+    #         "comment": {"body": "Hey, this ticket was assigned to you for resolution. Please follow up on it as soon as possible.\n\nCheers,\nYour Favourite Task Management Agent,\nPavlov\n\n"+description},
+    #         "requester": {
+    #             "name": requester_name,
+    #             "email": requester_email,
+    #         },
+    #         "assignee_id": assigneeID
+    #         # to assign it to someone in particular
+    #     }
+    # }
+    # if(assigneeID):
+    data={
+        "ticket": {
             "subject": subject,
             "comment": {"body": "Hey, this ticket was assigned to you for resolution. Please follow up on it as soon as possible.\n\nCheers,\nYour Favourite Task Management Agent,\nPavlov\n\n"+description},
             "requester": {
                 "name": requester_name,
                 "email": requester_email,
             },
-            # "assignee_id": assigneeID
-            # to assign it to someone in particular
+            "assignee_id": assigneeID
         }
     }
+    # else:
+    #     data={
+    #         "ticket": {
+    #             "subject": subject,
+    #             "comment": {"body": "Hey, this ticket was assigned to you for resolution. Please follow up on it as soon as possible.\n\nCheers,\nYour Favourite Task Management Agent,\nPavlov\n\n"+description},
+    #             "requester": {
+    #                 "name": requester_name,
+    #                 "email": requester_email,
+    #             },
+    #         }
+    #     }
 
     response = requests.post(
         url,
@@ -37,6 +64,13 @@ def create_zendesk_ticket(
         auth=(f"{EMAIL}/token", TOKEN),
         json=data,
     )
+    task={
+        "ID":response.json()['ticket']['id'],
+        "Status":"new",        
+        "Subject":subject
+    }
+    collection = db[assigneeEmail]
+    collection.insert_one(task)
     
     print(response.json())
 
@@ -49,8 +83,39 @@ def get_ticket_status(ticket_id):
     response = requests.get(
         url, auth=(f"{EMAIL}/token", TOKEN)
     )
-    print(response.json())
+    print(response.json()['ticket']['status'])
 
-    return response
+    return response.json()['ticket']['status']
 
-create_zendesk_ticket("Subject","Test","Siddhesh",EMAIL,0)
+
+def refreshAll(email):
+    collection = db[email]
+    all_documents = list(collection.find()) 
+    collection.delete_many({})
+    
+    for document in all_documents:
+        document['Status']=get_ticket_status(document['ID'])
+    
+    collection.insert_many(all_documents)
+    
+
+
+def get_agent_id_by_email(email):
+    url = f"https://{DOMAIN}.zendesk.com/api/v2/users/search.json?query={email}"
+    response = requests.get(url, auth=(f"{EMAIL}/token", TOKEN))
+    # print(response.json())
+    if response.status_code == 200:
+        users = response.json()["users"]
+        if users:
+            agent_id = users[0]["id"]
+            return agent_id
+        else:
+            print("No user found with the email:", email)
+            return None
+    else:
+        print(f"Error fetching user details: {response.status_code}")
+        return None
+
+
+create_zendesk_ticket("Subject","Test","Siddhesh","siddhesh.shrawne22@spit.ac.in","siddhesh.shrawne22@spit.ac.in")
+# print(get_agent_id_by_email("siddhesh.shrawne22@spit.ac.in"))
